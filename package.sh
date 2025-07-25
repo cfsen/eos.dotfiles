ec01db5477cc347a706c7a203993aa8a04bd265d
cfsen <103598332+cfsen@users.noreply.github.com>
2025-07-25 02:22:23 +0200

    update package.sh

--- package.sh
+++ package.sh
@@ -9,11 +9,18 @@ read
 mkdir -p ./hyprland/
 mkdir -p ./waybar/
 mkdir -p ./inori/
+mkdir -p ./rmpc/themes/
 mkdir -p ./ghostty/
 
 cp -v ~/.config/hypr/hyprland.conf ./hyprland/hyprland.conf
 cp -v ~/.config/hypr/hyprpaper.conf ./hyprland/hyprpaper.conf
+cp -v ~/.config/hypr/hyprlock.conf ./hyprland/hyprlock.conf
+
 cp -v ~/.config/waybar/styles.css ./waybar/styles.css
 cp -v ~/.config/waybar/config.jsonc ./waybar/config.jsonc
+
 cp -v ~/.config/inori/config.toml ./inori/config.toml
+cp -v ~/.config/rmpc/config.ron ./rmpc/config.ron
+cp -v ~/.config/rmpc/themes/theme.ron ./rmpc/themes/theme.ron
+
 cp -v ~/.config/ghostty/config ./ghostty/config
