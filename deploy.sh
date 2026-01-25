96b1351fc5bcb51f9296bd4eba3ed92e7bb210cd
cfsen <103598332+cfsen@users.noreply.github.com>
2026-01-25 10:03:07 +0100

    remove: package/deploy for rofi, waybar, inori

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,6 @@ echo "~/.config/hypr/hyprpaper.conf"
 echo "~/.config/hypr/hyprland.conf"
 echo "~/.config/hypr/hyprlock.conf"
 
-echo "~/.config/waybar/config.jsonc"
-echo "~/.config/waybar/styles.css"
-
-echo "~/.config/inori/config.toml"
 echo "~/.config/rmpc/config.ron"
 echo "~/.config/rmpc/themes/theme.ron"
 
@@ -28,24 +24,15 @@ echo "Press enter to continue."
 read
 
 mkdir -p ./.config/hypr/
-mkdir -p ./.config/waybar/
-mkdir -p ~/.config/inori/
 mkdir -p ~/.config/ghostty/
-mkdir -p ~/.config/rofi/
 
 cp -v ./hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
 cp -v ./hyprland/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
 cp -v ./hyprland/hyprlock.conf ~/.config/hypr/hyprlock.conf
 
-cp -v ./waybar/styles.css ~/.config/waybar/styles.css
-cp -v ./waybar/config.jsonc ~/.config/waybar/config.jsonc
-
-cp -v ./inori/config.toml ~/.config/inori/config.toml
 cp -v ./rmpc/config.ron ~/.config/rmpc/config.ron
 cp -v ./rmpc/themes/theme.ron ~/.config/rmpc/themes/theme.ron
 
 cp -v ./ghostty/config ~/.config/ghostty/config
-
-cp -v ./rofi/* ~/.config/rofi/
 
 cp -v ./.bashrc ~/.
--- package.sh
+++ package.sh
@@ -7,26 +7,17 @@ echo "Press enter to continue."
 read
 
 mkdir -p ./hyprland/
-mkdir -p ./waybar/
-mkdir -p ./inori/
 mkdir -p ./rmpc/themes/
 mkdir -p ./ghostty/
-mkdir -p ./rofi/
 mkdir -p ./bash/
 
 cp -v ~/.config/hypr/hyprland.conf ./hyprland/hyprland.conf
 cp -v ~/.config/hypr/hyprpaper.conf ./hyprland/hyprpaper.conf
 cp -v ~/.config/hypr/hyprlock.conf ./hyprland/hyprlock.conf
 
-cp -v ~/.config/waybar/styles.css ./waybar/styles.css
-cp -v ~/.config/waybar/config.jsonc ./waybar/config.jsonc
-
-cp -v ~/.config/inori/config.toml ./inori/config.toml
 cp -v ~/.config/rmpc/config.ron ./rmpc/config.ron
 cp -v ~/.config/rmpc/themes/theme.ron ./rmpc/themes/theme.ron
 
 cp -v ~/.config/ghostty/config ./ghostty/config
-
-cp -v ~/.config/rofi/* ./rofi
 
 cp -v ~/.bashrc ./bash
